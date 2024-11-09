05312d0e52e93177d6df0529d4c17aa8589e457e
dangerUser45 <danya541@bk.ru>
2024-11-09 19:29:10 +0300

    сделал первый dump graphviz

--- akinator.cpp
+++ akinator.cpp
@@ -6,14 +6,16 @@
 #include <assert.h>
 #include "akinator.h"
 
-FILE* Log_File;
-FILE* Graph_File;
+FILE* Log_File = 0;
+FILE* Graph_File = 0;
 
 //====================================================================================================================================
 int main ()
 {
-    Create_file ("LOG_AKINATOR.html", Log_File);
-    Create_file ("Dot.txt", Graph_File);
+    Log_File = Create_file ("LOG_AKINATOR.html");
+    Graph_File = Create_file ("Dot.txt");
+
+    fprintf (Log_File, "<pre>");
 
     node_akntr* node50_root = Create_node (50);
     node_akntr* node30 = Create_node (30);
@@ -32,6 +34,13 @@ int main ()
 
     Dump_in_line (node50_root);
     Dump_graph (node50_root);
+    Dump_graph (node10);
+
+    Close_File (Log_File);
+    Close_File (Graph_File);
+    system ("dot Dot.txt -Tpng -o tree.png");
+
+     txDisableAutoPause ();
 
     return 0;
 }
@@ -46,58 +55,92 @@ node_akntr* Create_node (el_t data)
     return new_node;
 }
 //====================================================================================================================================
-int  Create_file (const char* name_of_file, FILE* file)
+FILE* Create_file (const char* name_of_file)
 {
-    if ((file = fopen (name_of_file, "w+")) == NULL)
+    FILE* file = fopen ($(name_of_file), "w+");
+
+    if (file == NULL)
     {
-        fprintf (stdout, "Не удается открыть файл %s\n", name_of_file);
+        fprintf (stdout, "Не удается открыть файл %s", name_of_file);
+        perror (""); 
         return 0;
     }
+    
     setvbuf (file, 0, 0, _IONBF);
-    fprintf (file, "<pre>\n");
-    return 0;
+
+    return file;
+}
+//====================================================================================================================================
+FILE* Close_File (FILE* file)
+{
+    if (file) fclose (file); 
+    return file;
 }
 //====================================================================================================================================
 void Dump_graph (node_akntr* node)
 {
-    if (!node) return;
-    if (node -> right <= NULL)  {#define  node -> right -> data  0 
-                                 #define  node -> right -> left  0 
-                                 #define  node -> right -> right 0 };
-
-    if (node -> left <= NULL) {  #define  node -> left -> data   0 
-                                 #define  node -> left -> left   0 
-                                 #define  node -> left -> right  0 };       
+    if (!node) return;       
     fprintf(Graph_File, ""                                                                                                                  
                                                                                                                                               
-       "digraph" 
-    "{"
-       "rankdir = TB;"
+    "digraph\n" 
+    "{\n"
+        "\trankdir = TB;\n"
 
-        "{" 
-           "node[shape=plaintext];"
-         "edge[color=white]"
-         "\"1\" ->  \"2\";" 
-        "}"
+        "\t{\n"                                                         //
+            "\t\tnode[shape=plaintext];\n"
+            "\t\tedge[color=white]\n"
+            "\t\t\"1\" ->  \"2\";\n" 
+        "\t}\n\n"
  
-        "node [ color = \"#0d5a0e\", shape = \"rectangle\", style = \"filled\", fillcolor = \"#a2f8a4\"];"
-        "edge [color = \"blue\", fontsize = 16];");
-    fprintf(Graph_File, ""
-        "null    [ shape = \"Mrecord\", label = \" { data = %d\n addr: %p | { <left> L:\n addr: %p \n| <right> R: \n addr: %p \n} }\" ];", node -> data, node -> left, node -> right);
-    fprintf(Graph_File, ""
-        "first   [ shape = \"Mrecord\", label = \" { data = %d\n addr: %p | { <left> L:\n addr: %p \n| <right> R: \n addr: %p \n} }\" ];", node -> left -> data, node -> left -> left, node -> left -> right);
-    fprintf(Graph_File, ""   
-        "second  [ shape = \"Mrecord\", label = \" { data = %d\n addr: %p | { <left> L:\n addr: %p \n| <right> R: \n addr: %p \n} }\" ];", node -> right -> data, node -> right -> left, node -> right -> right);
- 
-    fprintf(Graph_File, ""
-        "null: <left> -> first;"
-        "null: <right> -> second;"
+        "\tnode [ color = \"#004b00\", shape = \"rectangle\", style = \"filled\", fillcolor = \"#a2f8a4\"];\n"
+        "\tedge [ color = \"#004b00\", fontsize = 16];\n\n");
 
-        "{ rank = same; \"1\"; \"null\" }"
-        "{ rank = same; \"2\"; \"first\"; \"second\";}"
-    "}"
+    fprintf(Graph_File, ""
+        "\tnull [ shape = \"Mrecord\", label = \"{ data = %d\\n addr: %p | { <left> L:\\n addr: %p | <right> R: \\n addr: %p } }\" ];\n", node -> data, node, node -> left, node -> right);
+    
+    if (node -> left != NULL)
+    {
+        fprintf(Graph_File, "\n"
+        "\tfirst[ shape = \"Mrecord\", label = \"{ data = %d\\n addr: %p | { <left> L:\\n addr: %p \\n| <right> R: \\n addr: %p \\n} }\" ];\n", node -> left -> data, node -> left, node -> left -> left, node -> left -> right);
+        fprintf (Graph_File, ""
+        "\tnull:<left> -> first;\n");
+    }
+    else {
+        fprintf(Graph_File, "\n"
+        "\t{\n"
+            "\t\tnode [ color = \"#007cff\", shape = \"rectangle\", style = \"filled\", fillcolor = \"#a2f0f8\"];\n"
+            "\t\tedge [ color = \"#007cff\", fontsize = 16];\n\n"
 
-    );
+            "\t\tfirst[ shape = \"Mrecord\", label = \"{ addr: %s | { <left> L:\\n addr: %s | <right> R: \\n addr: %s } }\" ];\n", "(null)",  "(null)", "(null)");
+        fprintf(Graph_File, ""
+            "\t\tnull:<left> -> first;\n"
+            "\t}\n");
+        }  
+
+    if (node -> right != NULL)
+    {
+        fprintf(Graph_File, "\n"   
+            "\t\tsecond  [ shape = \"Mrecord\", label = \" { data = %d\\n addr: %p | { <left> L:\\n addr: %p | <right> R: \\n addr: %p } }\" ];", node -> right -> data, node -> right, node -> right -> left, node -> right -> right);
+        fprintf(Graph_File, "\n"
+            "\t\tnull:<right> -> second;\n");
+    }
+    else {
+        fprintf(Graph_File, "\n"
+        "\t{\n"
+            "\t\tnode [ color = \"#007cff\", shape = \"rectangle\", style = \"filled\", fillcolor = \"#a2f0f8\"];\n"
+            "\t\tedge [ color = \"#007cff\", fontsize = 16];\n\n"
+
+            "\t\tsecond[shape = \"Mrecord\", label = \"{ addr: %s | { <left> L:\\n addr: %s | <right> R: \\n addr: %s } }\" ];\n", "(null)",  "(null)", "(null)");
+        
+        fprintf(Graph_File, ""
+            "\t\tnull:<right> -> second;\n"
+            "\t}\n");
+        }  
+
+        fprintf (Graph_File, "\n"
+            "\t{ rank = same; \"1\"; \"null\" }\n"
+            "\t{ rank = same; \"2\"; \"first\"; \"second\";}\n"
+        "}");
 }
 //====================================================================================================================================
 void Dump_in_line (node_akntr* node)
@@ -112,17 +155,5 @@ void Dump_in_line (node_akntr* node)
     fprintf (Log_File, ")");
 }
 //====================================================================================================================================
-
-struct node
-{
-    el_t data;
-    node* left;
-    node* right;
-};
- node* x = 0;
-
-x -> data;
-x -> right;
-x -> left;
 
 
